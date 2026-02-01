c766ea0adfb5cbdd458015c51df74ecf4a909a53
Yuri <yuri@heliconapps.com>
2026-02-01 14:42:56 +0100

    Introduce ProgressiveBlurView

---
+++ Sources/HeliconUI/View/ProgressiveBlurView.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  Helicon
+//
+//  Created by Yuriy Nefedov on 01.02.2026.
+//
+
+import SwiftUI
+
+public struct ProgressiveBlurView: View {
+    public var body: some View {
+        
+        Rectangle()
+            .fill(.ultraThinMaterial)
+            .mask {
+                VStack(spacing: 0) {
+                    LinearGradient(
+                        colors: [
+                            Color.black.opacity(0),
+                            Color.black.opacity(1),
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                    Rectangle()
+                }
+            }
+        
+    }
+}
+
+#Preview {
+    ProgressiveBlurView()
+}
