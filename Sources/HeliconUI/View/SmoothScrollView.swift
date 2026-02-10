372ad0032ff6ea219a701e6ea17b84afeeff1ac2
Yuri <yuri@heliconapps.com>
2026-02-10 09:10:17 +0100

    Optimize SmoothScrollView; introduce .blend mode

--- Sources/HeliconUI/View/SmoothScrollView.swift
+++ Sources/HeliconUI/View/SmoothScrollView.swift
@@ -7,57 +7,117 @@
 import SwiftUI
 
 public struct SmoothScrollView: View {
-    var marginHeight: CGFloat
-    var baseColor: Color
-    var edges: Edge.Set
-    var content: () -> any View
+    
+    public enum Mode {
+        case color(Color)
+        case blur
+        case blend
+    }
+    
+    let marginHeight: CGFloat
+    let applyPadding: Bool
+    let mode: Mode
+    let content: () -> any View
     
     public init(
-        marginHeight: CGFloat = 25,
-        baseColor: Color = .white,
-        edges: Edge.Set = [.top, .bottom],
+        mode: Mode = .blur,
+        marginHeight: CGFloat = 50,
+        applyPadding: Bool = true,
         content: @escaping () -> any View
     ) {
         
         self.marginHeight = marginHeight
-        self.baseColor = baseColor
-        self.edges = edges
+        self.applyPadding = applyPadding
+        self.mode = mode
         self.content = content
         
     }
     
     public var body: some View {
         ZStack {
-            ScrollView(.vertical, showsIndicators: false) {
-                AnyView(content())
-                    .padding(edges, marginHeight)
-            }
-            VStack {
-                
-                if edges.contains(.top) {
-                    ProgressiveBlurView()
-                        .rotationEffect(.degrees(180))
-                    .frame(height: marginHeight)
-                }
-                
-                Spacer()
-                
-                if edges.contains(.bottom) {
-                    ProgressiveBlurView(
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                    .frame(height: marginHeight)
+            Group {
+                switch mode {
+                case .blend:
+                    scrollViewContent
+                        .compositingGroup()
+                        .mask(seethroughMask)
+                default:
+                    scrollViewContent
                 }
             }
+            Group {
+                switch mode {
+                case .blur, .color:
+                    VStack {
+                        marginOverlay(isTop: true)
+                        Spacer()
+                        marginOverlay(isTop: false)
+                    }
+                default:
+                    EmptyView()
+                }
+            }
+        }
+    }
+    
+    private var scrollViewContent: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            AnyView(content())
+                .padding(.vertical, applyPadding ? marginHeight : 0)
+        }
+    }
+    
+    private func marginOverlay(isTop: Bool) -> some View {
+        Group {
+            switch mode {
+            case .blur:
+                ProgressiveBlurView()
+                    .rotationEffect(isTop ? .degrees(180) : .zero)
+                    .frame(height: marginHeight)
+            case .color(let c):
+                LinearGradient(
+                    colors: [c, c.opacity(0)],
+                    startPoint: isTop ? .top : .bottom,
+                    endPoint: isTop ? .bottom : .top
+                )
+                .frame(height: marginHeight)
+            default:
+                EmptyView()
+            }
+        }
+    }
+    
+    private var seethroughMask: some View {
+        VStack(spacing: 0) {
+            LinearGradient(colors: [Color.white.opacity(0), Color.white], startPoint: .top, endPoint: .bottom)
+                .frame(height: marginHeight)
+            Rectangle()
+                .fill(Color.white)
+            LinearGradient(colors: [Color.white, Color.white.opacity(0)], startPoint: .top, endPoint: .bottom)
+                .frame(height: marginHeight)
         }
     }
 }
 
 #Preview {
-    SmoothScrollView {
-        Color.indigo
-            .frame(height: 2000)
+    ZStack {
+//        Color.red.ignoresSafeArea()
+        SmoothScrollView(
+            mode: .blend,
+            marginHeight: 200,
+            applyPadding: true
+        ) {
+//            Color.indigo
+//                .frame(height: 2000)
+            ForEach(0...30, id: \.self) { index in
+                
+                let fontSize: CGFloat = .init(50 - index)
+                
+                Text("Hello, world")
+                    .font(.system(size: fontSize))
+            }
+        }
+//        .ignoresSafeArea()
     }
-    .padding()
 }
+
